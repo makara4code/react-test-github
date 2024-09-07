6292d3eaceea7817de05ef3d5388cd05b6efb2ef
MAKARASAMA\Admin <nuolmakara@gmail.com>
2024-09-07 15:55:09 +0700

    My First Commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+
+import axios from "axios";
+
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchTodos = async () => {
+    setLoading(true);
+    const response = await axios.get("https://jsonplaceholder.typicode.com/todos");
+    setLoading(false);
+
+    setTodos(response.data);
+  };
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  return (
+    <>
+      <h1>Todos</h1>
+
+      {loading ? (
+        <p>Loading data</p>
+      ) : (
+        <ol>
+          {todos.map((todo) => (
+            <li key={todo.id}>{todo.title}</li>
+          ))}
+        </ol>
+      )}
+    </>
+  );
+};
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import App from './App.jsx'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+createRoot(document.getElementById('root')).render(
+    <App />
+)
